a9b665f6173782be14b6c3149525ec40ef282810
Phu Nguyen Duong Thanh <1234hdpa@gmail.com>
2023-01-13 13:39:25 +0700

    - Update renaming method for both entry - Update build.rs for request administration right when starting

--- build.rs
+++ build.rs
@@ -1,9 +1,34 @@
 extern crate winres;
 
 fn main() {
-  if cfg!(target_os = "windows") {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("1.ico"); // Replace this with the filename of your .ico file.
-    res.compile().unwrap();
-  }
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("1.ico"); // Replace this with the filename of your .ico file.
+        res.compile().unwrap();
+        use std::io::Write;
+        // only build the resource for release builds
+        // as calling rc.exe might be slow
+
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("1.ico").set_manifest(
+            r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#,
+        );
+        match res.compile() {
+            Err(error) => {
+                write!(std::io::stderr(), "{}", error).unwrap();
+                std::process::exit(1);
+            }
+            Ok(_) => {}
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![allow(non_snake_case)]
 
-
 use directories::BaseDirs;
-use std::{path::Path, ptr::null_mut as NULL, process::{self}, fs::{self}, io};
+use std::{
+    fs::{self},
+    io,
+    path::Path,
+    process::{self},
+    ptr::null_mut as NULL,
+};
 use winapi::um::winuser::{self, IDYES};
 
 
@@ -16,7 +21,9 @@ fn main() {
         //Nhập ổ đĩa muốn chuyển
         println!("Nhap o dia muon chuyen ZaloPC sang (VD: D hoac E): ");
         let mut name = String::new();
-        io::stdin().read_line(&mut name).expect("Failed To read Input");
+        io::stdin()
+            .read_line(&mut name)
+            .expect("Failed To read Input");
         let name_driver = name.trim();
         //let len = name_driver.len();
         //name_driver.truncate(len - 1);
@@ -28,17 +35,37 @@ fn main() {
         println!("ZaloPC se duoc chuyen den {}!", base.display());
         if b.exists() {
             let box_title: Vec<u16> = base_path.encode_utf16().collect();
-            let box_msg_ask: Vec<u16> = "Phát hiện ZaloPC ở ổ C! Bạn có muốn tiếp tục?\0".encode_utf16().collect();
-            let box_msg_confirm: Vec<u16> = "Bắt đầu chuyển ZaloPC sang ổ chỉ định!\0".encode_utf16().collect();
+            let box_msg_ask: Vec<u16> = "Phát hiện ZaloPC ở ổ C! Bạn có muốn tiếp tục?\0"
+                .encode_utf16()
+                .collect();
+            let box_msg_confirm: Vec<u16> = "Bắt đầu chuyển ZaloPC sang ổ chỉ định!\0"
+                .encode_utf16()
+                .collect();
             let box_msg_cancel: Vec<u16> = "Đã huỷ việc chuyển dữ liệu!\0".encode_utf16().collect();
-            let box_msg_complete: Vec<u16> = "Chuyển ZaloPC sang ổ chỉ định thành công!\0".encode_utf16().collect();
+            let box_msg_complete: Vec<u16> = "Chuyển ZaloPC sang ổ chỉ định thành công!\0"
+                .encode_utf16()
+                .collect();
             unsafe {
-                let msgbox_id = winuser::MessageBoxW(NULL(), box_msg_ask.as_ptr(), box_title.as_ptr(), winuser::MB_YESNO | winuser::MB_ICONEXCLAMATION);
+                let msgbox_id = winuser::MessageBoxW(
+                    NULL(),
+                    box_msg_ask.as_ptr(),
+                    box_title.as_ptr(),
+                    winuser::MB_YESNO | winuser::MB_ICONEXCLAMATION,
+                );
                 if msgbox_id == IDYES {
-                    winuser::MessageBoxW(NULL(), box_msg_confirm.as_ptr(), box_title.as_ptr(), winuser::MB_OK | winuser::MB_ICONINFORMATION);
-                }
-                else {
-                    winuser::MessageBoxW(NULL(), box_msg_cancel.as_ptr(), box_title.as_ptr(), winuser::MB_OK | winuser::MB_ICONINFORMATION);
+                    winuser::MessageBoxW(
+                        NULL(),
+                        box_msg_confirm.as_ptr(),
+                        box_title.as_ptr(),
+                        winuser::MB_OK | winuser::MB_ICONINFORMATION,
+                    );
+                } else {
+                    winuser::MessageBoxW(
+                        NULL(),
+                        box_msg_cancel.as_ptr(),
+                        box_title.as_ptr(),
+                        winuser::MB_OK | winuser::MB_ICONINFORMATION,
+                    );
                     process::exit(0);
                 }
             }
@@ -48,18 +75,22 @@ fn main() {
             copy_dir_all(&b, &backup).expect("Error in copying files!");
             println!(" Xong!");
             //Thực hiện xoá ZaloPC ở ổ C
-            println!("Dang xoa thu muc ZaloPC goc o duong dan {}... ", b.display());
+            println!(
+                "Dang xoa thu muc ZaloPC goc o duong dan {}... ",
+                b.display()
+            );
             fs::remove_dir_all(&b).expect("Error in deleting ZaloPC in driver C!");
             println!(" Xong!");
             //Thực hiện mklink cho ZaloPC
             println!("Dang tao lenh mklink ve thu muc ZaloPC goc...");
             std::process::Command::new("cmd")
-            .arg("/C")
-            .arg("mklink")
-            .arg("/d")
-            .arg(&b)
-            .arg(&base_path)
-            .output().expect("Error in making mklink!");
+                .arg("/C")
+                .arg("mklink")
+                .arg("/d")
+                .arg(&b)
+                .arg(&base_path)
+                .output()
+                .expect("Error in making mklink!");
             println!(" Xong!");
             println!("Dang thuc hien chuyen ZaloBackup thanh ZaloPC...");
             //Kiểm tra và xoá thư mục ZaloPC đã bị trùng trước đó
@@ -69,28 +100,42 @@ fn main() {
             }
             //Thực hiện đổi tên Backup thành ZaloPC
             std::process::Command::new("cmd")
-            .arg("/C")
-            .arg("REN")
-            .arg(&backup)
-            .arg("ZaloPC")
-            .output().expect("Error in renaming folder!");
-            //fs::rename(&backup_path, &base_path).expect("Error in renaming BackupZalo to ZaloPC!");
+                .arg("/C")
+                .arg("REN")
+                .arg(&backup)
+                .arg("ZaloPC")
+                .output()
+                .expect("Error in renaming folder!");
+            if !(base.exists()) {
+                fs::rename(&backup_path, &base_path).expect("Error in renaming BackupZalo to ZaloPC!");
+            }
             println!(" Xong!");
             unsafe {
-                winuser::MessageBoxW(NULL(), box_msg_complete.as_ptr(), box_title.as_ptr(), winuser::MB_OK | winuser::MB_ICONINFORMATION);
+                winuser::MessageBoxW(
+                    NULL(),
+                    box_msg_complete.as_ptr(),
+                    box_title.as_ptr(),
+                    winuser::MB_OK | winuser::MB_ICONINFORMATION,
+                );
+                process::exit(0);
+            }
+        } else {
+            let box_msg_cancel: Vec<u16> =
+                "Đã huỷ việc chuyển đổi! \n Không phát hiện Zalo trên ổ C!\0"
+                    .encode_utf16()
+                    .collect();
+            let box_title: Vec<u16> = "ZaloPC không tồn tại!\0".encode_utf16().collect();
+            unsafe {
+                winuser::MessageBoxW(
+                    NULL(),
+                    box_msg_cancel.as_ptr(),
+                    box_title.as_ptr(),
+                    winuser::MB_OK | winuser::MB_ICONINFORMATION,
+                );
                 process::exit(0);
             }
         }
-        else {
-            let box_msg_cancel: Vec<u16> = "Đã huỷ việc chuyển đổi! \n Không phát hiện Zalo trên ổ C!\0".encode_utf16().collect();
-            let box_title: Vec<u16> = "ZaloPC không tồn tại!\0".encode_utf16().collect();
-            unsafe {
-                winuser::MessageBoxW(NULL(), box_msg_cancel.as_ptr(), box_title.as_ptr(), winuser::MB_OK | winuser::MB_ICONINFORMATION);
-                process::exit(0);
-        }
-        }
     }
-    
 }
 fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
     fs::create_dir_all(&dst)?;
